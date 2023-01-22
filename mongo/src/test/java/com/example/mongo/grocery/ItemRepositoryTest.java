d3124485f026209b03729487159dd11be5049058
seokrae <kslbsh@gmail.com>
2023-01-22 21:22:14 +0900

    feat: @DataMongoTest는 Transactional을 포함하고 있지 않음 테스트에 대해 독립적인 환경을 만들기 위해서는 임베디드를 쓰던지 해야 할듯

--- mongo/src/test/java/com/example/mongo/grocery/ItemRepositoryTest.java
+++ mongo/src/test/java/com/example/mongo/grocery/ItemRepositoryTest.java
@@ -1,5 +1,8 @@
 package com.example.mongo.grocery;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
@@ -13,13 +16,24 @@ class ItemRepositoryTest {
 	@Autowired
 	private ItemRepository itemRepository;
 	
-	@Test
-	void testCase1() {
+	@BeforeEach
+	void setUp() {
 		itemRepository.save(new GroceryItem("1", "Milk", 1, "Dairy"));
-		long count = itemRepository.count();
-		System.out.println("count = " + count);
 	}
 	
+	@AfterEach
+	void tearDown() {
+		itemRepository.deleteAll();
+	}
+	
+	@DisplayName("몽고 DB 데이터 저장 및 건수 확인 테스트")
+	@Test
+	void testCase1() {
+		long count = itemRepository.count();
+		assertThat(count).isOne();
+	}
+	
+	@DisplayName("몽고 DB 데이터 저장 및 조회 데이터 동일성 테스트")
 	@Test
 	void testCase2() {
 		GroceryItem milk = itemRepository.findItemByName("Milk");
